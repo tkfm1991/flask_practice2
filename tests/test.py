6bd86ece1e416cc5d31b3f71b9c114de8bb530c6
tkfm1991 <kuroro1991@gmail.com>
2020-11-21 14:08:20 +0900

    update 機能修正

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ from importlib import import_module
 
 
 class FlaskTestCase(unittest.TestCase):
-    def test_get_ok(self):
+    def test_get1_ok(self):
         sys.path.append('..')
         flask_app = import_module('main1')
         flask_app.app.testing = True
@@ -12,7 +12,7 @@ class FlaskTestCase(unittest.TestCase):
             result = c.get('/result?fruit_no=1')
             assert 'もも' in result.get_data(as_text=True)
 
-    def test_get_ng(self):
+    def test_get1_ng(self):
         sys.path.append('..')
         flask_app = import_module('main1')
         flask_app.app.testing = True
@@ -22,3 +22,14 @@ class FlaskTestCase(unittest.TestCase):
                 result = c.get('/result?fruit_no=5')
 
             self.assertEqual(e.exception.args[0], '5')
+
+    def test_get3_ok(self):
+        sys.path.append('..')
+        flask_app = import_module('main3')
+        flask_app.app.testing = True
+        with flask_app.app.test_client() as c:
+            name = 'Suzuki'
+            actual = c.get('/output/1/', query_string=dict(name=name))
+            assert actual.status_code == 200
+            assert name in actual.get_data(as_text=True)
+            assert 'もも' in actual.get_data(as_text=True)
